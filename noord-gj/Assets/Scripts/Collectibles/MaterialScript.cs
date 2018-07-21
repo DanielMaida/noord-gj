795aecfded97a5ba90dc6c8599af01fa2c86f7f6
DanielMaida <dfm2@cin.ufpe.br>
2018-07-20 23:28:20 -0300

    movimentacao do barco e sistema de pickup

---
+++ noord-gj/Assets/Scripts/Collectibles/MaterialScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialScript : MonoBehaviour {
+    
+    public float BlinkRepetitions;
+
+    private CircleCollider2D MyCircleCollider;
+    private SpriteRenderer MySpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        MyCircleCollider = GetComponent<CircleCollider2D>();
+        MySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(PickupCoroutine());
+        }
+    }
+
+    IEnumerator PickupCoroutine()
+    {
+        for(int i = 0; i < BlinkRepetitions; i++)
+        {
+            MySpriteRenderer.color = Color.red;
+            yield return new WaitForSeconds(0.5f);
+            MySpriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(0.5f);
+        }
+        Destroy(gameObject);
+    }
+    
+}
---
+++ noord-gj/Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    [Header("Movement components")]
+    public float ThrustAxis;
+    public float RotationAxis;
+    public float Angle;
+
+    [Header("Player Parameters")]
+    public float Speed;
+
+    private Rigidbody2D MyRigidbody2D;
+
+	// Use this for initialization
+	void Start () {
+        MyRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        MyRigidbody2D.velocity = transform.up * Speed * ThrustAxis;
+
+        if (RotationAxis != 0)
+        {
+            MyRigidbody2D.MoveRotation(MyRigidbody2D.rotation + RotationAxis);
+        }
+    }
+
+    private void CheckMovement()
+    {
+        RotationAxis = Input.GetAxisRaw("Horizontal");
+        ThrustAxis = Input.GetAxisRaw("Vertical");
+    }
+
+}
